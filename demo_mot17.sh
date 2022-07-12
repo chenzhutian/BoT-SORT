81d4db9f7e5d0b1c43462eac8b722fcde4434d0a
ztchen <zhutian.chen.code@outlook.com>
2022-07-12 00:46:14 +0000

    init

---
+++ demo_mot17.sh
@@ -0,0 +1,1 @@
+python3 tools/demo.py video --path videos/22_,.mp4 -f yolox/exps/example/mot/yolox_x_mix_det.py -c pretrained/bytetrack_x_mot17.pth.tar --with-reid --fuse-score --fp16 --fuse --save_result
---
+++ demo_mot20.sh
@@ -0,0 +1,1 @@
+python3 tools/demo.py video --path videos/22_,.mp4 -f yolox/exps/example/mot/yolox_x_mix_det.py -c pretrained/bytetrack_x_mot20.tar --with-reid --fuse-score --fp16 --fuse --save_result
