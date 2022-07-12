89e57f052d76ebdcb524098daa65ba041c56cc44
ztchen <zhutian.chen.code@outlook.com>
2022-07-12 19:12:32 +0000

    demo script

--- demo_mot17_gt.sh
+++ demo_mot17_gt.sh
@@ -1,5 +1,5 @@
 python3 tools/demo.py video --path videos/22_,.mp4 \
 -f yolox/exps/example/mot/yolox_x_mix_det.py \
 -c pretrained/bytetrack_x_mot17.pth.tar \
--g labels/bboxes/22_,10.json \
+-g labels/bboxes/22_,.json \
 --with-reid --fuse-score --fp16 --fuse --save_result
